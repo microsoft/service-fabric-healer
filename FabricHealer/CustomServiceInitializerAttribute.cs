9e2ab7ba0601bcc3e9af70c0b65e5e91e5c40fa0
Naresh Kumar <kumarnaresh@microsoft.com>
2024-02-08 20:38:36 +0530

    attribute for plugin assembly loading

---
+++ FabricHealer/CustomServiceInitializerAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FabricHealer
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class CustomServiceInitializerAttribute: Attribute
+    {
+        public Type InitializerType { get; }
+
+        public CustomServiceInitializerAttribute(Type startupType)
+        {
+            this.InitializerType = startupType;
+        }
+    }
+}
