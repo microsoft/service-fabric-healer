e31b63d55b2ebe29cbb2e2e9dd594b6d0e213c2b
Naresh Kumar <kumarnaresh@microsoft.com>
2024-03-02 00:58:22 +0530

    sample healer plugin project like the one we have in FO

---
+++ SampleHealerPlugin/SampleNewHealerStartup.cs
@@ -0,0 +1,23 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using FabricHealer.Attributes;
+using FabricHealer.Interfaces;
+using FabricHealer.Repair.Guan;
+using FabricObserver.Observers;
+using Guan.Logic;
+using FabricHealer.Utilities.Telemetry;
+
+[assembly: CustomRepairPredicateType(typeof(SampleNewHealerStartup))]
+namespace FabricObserver.Observers
+{
+    public class SampleNewHealerStartup : IPredicateTypesCollection
+    {
+        public void RegisterPredicateTypes(FunctorTable functorTable, TelemetryData repairData)
+        {
+            functorTable.Add(SampleHealerPluginPredicateType.Singleton(nameof(SampleHealerPluginPredicateType), repairData));
+        }
+    }
+}
