a9d49f28d33430664d5aea4bb1bf8dec5c825ad3
Naresh Kumar <kumarnaresh@microsoft.com>
2024-03-02 00:51:19 +0530

    attribute to instantiate plugin classes

---
+++ FabricHealer/Attributes/CustomRepairPredicateTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FabricHealer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class CustomRepairPredicateTypeAttribute : Attribute
+    {
+        public Type CustomRepairPredicateType { get; }
+
+        public CustomRepairPredicateTypeAttribute(Type className)
+        {
+            CustomRepairPredicateType = className;
+        }
+    }
+}
