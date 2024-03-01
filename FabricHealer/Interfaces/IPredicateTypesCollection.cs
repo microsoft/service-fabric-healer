d0ca1f4f16cfaa0223db54b4a70a40d870970d1a
Naresh Kumar <kumarnaresh@microsoft.com>
2024-03-02 00:51:39 +0530

    interface plugins shud implement

---
+++ FabricHealer/Interfaces/IPredicateTypesCollection.cs
@@ -0,0 +1,10 @@
+﻿using FabricHealer.Utilities.Telemetry;
+using Guan.Logic;
+
+namespace FabricHealer.Interfaces
+{
+    public interface IPredicateTypesCollection
+    {
+        void RegisterPredicateTypes(FunctorTable functorTable, TelemetryData repairData);
+    }
+}
