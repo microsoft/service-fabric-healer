7e4605bf10e3f9835392130ead2de641c6908e70
Naresh Kumar <kumarnaresh@microsoft.com>
2024-03-02 14:39:49 +0530

    add custom service initializer too to the sample project

---
+++ SampleHealerPlugin/CustomServiceInitializer.cs
@@ -0,0 +1,15 @@
+﻿using FabricHealer.SamplePlugins;
+using FabricHealer.Attributes;
+using FabricHealer.Interfaces;
+
+[assembly: CustomServiceInitializer(typeof(CustomServiceInitializer))]
+namespace FabricHealer.SamplePlugins
+{
+    public class CustomServiceInitializer : ICustomServiceInitializer
+    {
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
