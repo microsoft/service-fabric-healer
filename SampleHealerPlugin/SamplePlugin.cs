997eadf49d3b8c78089d802ec753074ed71a3944
Naresh Kumar <kumarnaresh@microsoft.com>
2024-08-04 23:37:00 +0530

    add sample plugin for new plugin contracts

---
+++ SampleHealerPlugin/SamplePlugin.cs
@@ -0,0 +1,38 @@
+﻿using System.Fabric;
+using FabricHealer;
+using FabricHealer.Interfaces;
+using FabricHealer.SamplePlugins;
+using FabricHealer.Utilities;
+using FabricHealer.Utilities.Telemetry;
+using SampleHealerPlugin;
+
+[assembly: Plugin(typeof(SamplePlugin))]
+namespace SampleHealerPlugin
+{
+    /// <summary>
+    /// Sample plugin for FabricHealer.
+    ///  Implemented using the new Fabric healer plugin contracts.
+    /// </summary>
+    internal class SamplePlugin : IPlugin
+    {
+        public Task InitializeAsync(ServiceContext serviceContext, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// sample plugin implements this method to deserialize the repair data to a custom type.
+        public T DeserializeRepairData<T>(string json) where T : TelemetryData
+        {
+            JsonSerializationUtility.TryDeserializeObject(json, out SampleTelemetryData repairData);
+            return repairData as T;
+        }
+
+        public IReadOnlyDictionary<string, Type> GetPredicateTypes()
+        {
+            return new Dictionary<string, Type>
+            {
+                { "SampleRepairV2", typeof(SamplePredicateType) }
+            };
+        }
+    }
+}
