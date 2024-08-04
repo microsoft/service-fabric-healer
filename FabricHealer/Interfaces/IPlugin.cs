c0c51f7423a2e47b05d287e9a6aac512b46261cf
Naresh Kumar <kumarnaresh@microsoft.com>
2024-08-04 19:21:30 +0530

    new contract changes

---
+++ FabricHealer/Interfaces/IPlugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Threading.Tasks;
+using FabricHealer.Utilities;
+using FabricHealer.Utilities.Telemetry;
+
+namespace FabricHealer.Interfaces
+{
+    /// <summary>
+    /// Has to be implemented if either EnableCustomServiceInitializers or EnableCustomRepairPredicateType application parameters are set.
+    /// The implementation should also use the attribute - <see cref="PluginAttribute"/>.
+    /// FH creates singleton instances of the implemented plugin classes.
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Has to be implemented if EnableCustomServiceInitializers application parameter is set.
+        /// Will be executed once at the beginning of the service.
+        /// </summary>
+        /// <param name="serviceContext">Fabric healer's service context</param>
+        /// <returns></returns>
+        Task InitializeAsync(ServiceContext serviceContext);
+
+        /// <summary>
+        /// Optional
+        /// Has to be overridden if plugins need custom deserialization of the repair data.
+        /// Will be executed during every repair workflows if EnableCustomRepairPredicateType application parameter is set.
+        /// By default, it will deserialize the repair data to <see cref="TelemetryData"/>.
+        /// </summary>
+        /// <typeparam name="T">Deserialization target type. A class that inherits from <see cref="TelemetryData"/></typeparam>
+        /// <param name="json">Repair info present in the SF Health report</param>
+        /// <returns>Deserialized repair data</returns>
+        virtual T DeserializeRepairData<T>(string json) where T : TelemetryData
+        {
+            JsonSerializationUtility.TryDeserializeObject(json, out TelemetryData repairData);
+            return (T)repairData;
+        }
+
+        /// <summary>
+        /// Has to be implemented if EnableCustomRepairPredicateType application parameter is set.
+        /// Will be executed once at the beginning of the service.
+        /// </summary>
+        /// <returns>
+        /// A map of predicate names and their respective types.
+        /// types should be classes that inherit from <see cref="IPredicate"/> and <see cref="PredicateType"/>
+        /// </returns>
+        IReadOnlyDictionary<string, Type> GetPredicateTypes();
+    }
+}
---
+++ FabricHealer/Interfaces/IPredicate.cs
@@ -0,0 +1,17 @@
+﻿using FabricHealer.Utilities.Telemetry;
+
+namespace FabricHealer.Interfaces;
+
+/// <summary>
+/// Has to be implemented by the predicate types in the plugins, if EnableCustomRepairPredicateType application parameter is set.
+/// FH creates singleton instances of the implemented predicate classes.
+/// </summary>
+public interface IPredicate
+{
+    /// <summary>
+    /// Will be executed during every repair workflows.
+    /// </summary>
+    /// <typeparam name="T">Repair info type</typeparam>
+    /// <param name="repairData">Repair info to be passed into the predicate</param>
+    void SetRepairData<T>(T repairData) where T : TelemetryData;
+}
